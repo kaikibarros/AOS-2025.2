befc9becb951c31ce5be6c3671ac23c9a82fb4cb
Kaiki Barros <104598739+kaikibarros@users.noreply.github.com>
2025-09-26 15:12:00 +0000

    adição da rota

--- atividade-tarefas/api/app.js
+++ atividade-tarefas/api/app.js
@@ -8,6 +8,8 @@ import tarefas from './models/Tarefa.js';
 
 const app = express();
 
+
+
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -20,6 +22,8 @@ app.use((req, res, next) => {
   };
   next();
 });
+app.get('/', (req, res) => res.send('<h1>API de Tarefas</h1>'));
+
 app.use('/tarefas', tarefasRoutes);
 
 const PORT = process.env.PORT || 3000;
@@ -28,43 +32,3 @@ app.listen(PORT, () => {
   console.log(`Server rodando na porta: ${PORT}`);
 });
 
-
-/*A API deve fornecer os seguintes endpoints:
-
- POST /tarefas: Cria uma nova tarefa.
-
- Corpo da Requisição: { "descricao": "String", "concluida": "boolean" }
-
- Resposta: A tarefa criada, incluindo o objectId gerado. O objectId deve ser um UUID gerado pelo back-end. A descricao é obrigatória, e concluida é opcional, com valor padrão false.
-
- Validação: Se a descricao não for fornecida, a API deve retornar um erro.
-
- GET /tarefas: Lista todas as tarefas cadastradas.
-
- Resposta: Um array de objetos de tarefas.
-
- GET /tarefas/:objectId: Retorna uma tarefa específica pelo seu ID.
-
- Parâmetros: objectId (UUID da tarefa).
-
- Resposta: O objeto da tarefa encontrada.
-
- Resposta de Erro: Se a tarefa não for encontrada, retorna um erro 404 Not Found.
-
- PUT /tarefas/:objectId: Atualiza uma tarefa existente.
-
- Parâmetros: objectId (UUID da tarefa).
-
- Corpo da Requisição: { "descricao": "String", "concluida": "boolean" } (opcionalmente um ou ambos).
-
- Resposta: O objeto da tarefa atualizada.
-
- Validação: Se o objectId não for encontrado, a API deve retornar um erro 404 Not Found.
-
- DELETE /tarefas/:objectId: Remove uma tarefa.
-
-Parâmetros: objectId (UUID da tarefa).
-
- Resposta: Uma mensagem de sucesso ou um objeto vazio.
-
- Resposta de Erro: Se a tarefa não for encontrada, retorna um erro 404 Not Found. */
